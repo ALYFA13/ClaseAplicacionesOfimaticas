95062d8030c5de1b2002c97db7f7cdc0ea74a2ff
ALYFA13 <f122333444455555666666f123456@gmail.com>
2022-09-27 10:16:59 +0200

    hola1

--- 27_09_22_APP1/27_09_22_APP1/Module1.vb
+++ 27_09_22_APP1/27_09_22_APP1/Module1.vb
@@ -2,11 +2,27 @@
 
     Sub Main()
 
+        'calculamos la hipotenusa en triángulo rectángulo
+        Dim lado1, lado2, h As Double
 
+        Console.WriteLine("¿Lado 1?")
+        lado1 = Convert.ToDouble(Console.ReadLine())
+        Console.WriteLine("¿Lado 2?")
+        lado2 = Convert.ToDouble(Console.ReadLine())
 
+        h = (lado1 * lado1 + lado2 * lado2) ^ (1 / 2)
+        'el simbolo "^", se puede sustituir por "Math.Pow(lado1,2)" (primero ponemos el nº y luego a cuanto lo elevamos)
+        'y la raiz por "Math.Sqrt()"
+
+        'Debemos redondear a las centésimas, usaremos "Math.Round(h,2)"
+        Console.WriteLine("Hipotenusa = " & Math.Round(h, 2)) 'no se guarda la aproximación para más tarde
+        Console.WriteLine("Hipotenusa = " & h) 'aquí lo vemos
+        h = Math.Round(h, 2) 'se guarda la aproximación para más tarde
+        Console.WriteLine("Hipotenusa = " & h) 'aquí lo vemos
 
 
         Console.ReadLine()
+
     End Sub
 
 End Module
---
+++ 27_9_22_APP2/27_9_22_APP2/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim precI, precF As Single
+        Dim descuento As Byte 'porq no va a pasar del 100% el porcentaje
+        Convert.ToSingle(descuento)
+
+        Console.WriteLine("¿Precio inicial?")
+        precI = Convert.ToSingle(Console.ReadLine())
+        Console.WriteLine("¿Descuento?")
+        descuento = Convert.ToByte(Console.ReadLine())
+
+        precF = Convert.ToSingle(precI - (precI * descuento / 100))
+        Console.WriteLine("Precio final: " & precF)
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
